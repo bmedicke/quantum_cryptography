58be4ca97db3c8765fba89361ee2f498a4c20489
Benjamin Medicke <bmedicke@gmail.com>
2021-05-13 18:28:27 +0100

    fix formatting

--- notebooks/qcrypt/hardware/laser.py
+++ notebooks/qcrypt/hardware/laser.py
@@ -1,11 +1,13 @@
 import time
 from . import relay_lib_seeed
 
-DELAY = 1 # in seconds.
+DELAY = 1  # in seconds.
 RELAY = 1
 
+
 def hello():
-    print('pew pew pew!')
+    print("pew pew pew!")
+
 
 def trigger():
     relay_lib_seeed.relay_on(RELAY)
