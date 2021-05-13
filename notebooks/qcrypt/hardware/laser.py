6cbd53dcf2d79a3f50fb1bd24027e573c3f1c506
Benjamin Medicke <bmedicke@gmail.com>
2021-05-13 18:12:17 +0100

    default to relay 1

--- notebooks/qcrypt/hardware/laser.py
+++ notebooks/qcrypt/hardware/laser.py
@@ -2,11 +2,12 @@ import time
 from . import relay_lib_seeed
 
 DELAY = 1 # in seconds.
+RELAY = 1
 
 def hello():
     print('pew pew pew!')
 
 def trigger():
-    relay_lib_seeed.relay_on(4)
+    relay_lib_seeed.relay_on(RELAY)
     time.sleep(DELAY)
-    relay_lib_seeed.relay_off(4)
+    relay_lib_seeed.relay_off(RELAY)
