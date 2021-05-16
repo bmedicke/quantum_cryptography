f00125d54b1c4d5a4ce0df6bd68ca4bcd1325948
Benjamin Medicke <bmedicke@gmail.com>
2021-05-16 20:04:03 +0100

    use config dict

--- notebooks/qcrypt/hardware/laser.py
+++ notebooks/qcrypt/hardware/laser.py
@@ -3,22 +3,22 @@ import logging
 import paho.mqtt.client as mqtt
 from . import relay_lib_seeed
 
-
-name = None
-relay = None
-delay = None
-laser_channel = None
+config = {
+    "name": None,
+    "relay": None,
+    "delay": None,
+    "laser_channel": None,
+}
 
 
 def init(username="alice", relay_id=1, delay_in_seconds=1):
-    global name
-    global relay
-    global delay
-    global laser_channel
-    name = username
-    relay = relay_id
-    delay = delay_in_seconds
-    laser_channel = f"quantum_cryptography/classical_channel/{name}/laser"
+    global config
+    config["name"] = username
+    config["relay"] = relay_id
+    config["delay"] = delay_in_seconds
+    config[
+        "laser_channel"
+    ] = f"quantum_cryptography/classical_channel/{username}/laser"
     logging.info(f"relay: selected number {relay_id}")
     logging.info(f"delay between on and off: {delay_in_seconds}s")
     client.connect(mqtt_broker_ip, 1883, 60)
@@ -26,26 +26,26 @@ def init(username="alice", relay_id=1, delay_in_seconds=1):
 
 
 def on_connect(client, userdata, flags, rc):
-    global name
-    logging.info(f"mqtt: {name} connected")
+    global config
+    logging.info(f"mqtt: {config['name']} connected")
 
 
 def on_disconnect(client, userdata, rc):
-    global name
-    logging.warning(f"mqtt: {name} disconnected")
+    global config
+    logging.warning(f"mqtt: {config['name']} disconnected")
 
 
 def trigger():
-    global delay
-    client.publish(laser_channel, payload="on", qos=0, retain=False)
-    relay_lib_seeed.relay_on(relay)
-    logging.info(f"laser relay {relay}: on")
+    global config
+    client.publish(config["laser_channel"], payload="on", qos=0, retain=False)
+    relay_lib_seeed.relay_on(config["relay"])
+    logging.info(f"laser relay {config['relay']}: on")
 
-    time.sleep(delay)
+    time.sleep(config["delay"])
 
-    client.publish(laser_channel, payload="off", qos=0, retain=False)
-    relay_lib_seeed.relay_off(relay)
-    logging.info(f"laser relay {relay}: off")
+    client.publish(config["laser_channel"], payload="off", qos=0, retain=False)
+    relay_lib_seeed.relay_off(config["relay"])
+    logging.info(f"laser relay {config['relay']}: off")
 
 
 logging.basicConfig(
