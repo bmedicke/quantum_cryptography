8f5274f7206743d3b4c9c91f537d8e4e90505700
Benjamin Medicke <bmedicke@gmail.com>
2021-05-16 21:15:08 +0100

    make laser a class

--- notebooks/qcrypt/hardware/laser.py
+++ notebooks/qcrypt/hardware/laser.py
@@ -4,67 +4,59 @@ import paho.mqtt.client as mqtt
 from . import relay_lib_seeed
 
 
-class Config:
-    pass
+class Laser:
+    def __init__(
+        self,
+        username="alice",
+        relay_id=1,
+        delay_in_seconds=1,
+        mqtt_broker_ip="localhost",
+    ):
 
+        self.name = username
+        self.relay = relay_id
+        self.delay = delay_in_seconds
+        self.laser_channel = (
+            f"quantum_cryptography/classical_channel/{username}/laser"
+        )
 
-_config = Config()
-_config.name = None
-_config.relay = None
-_config.delay = None
-_config.laser_channel = None
+        logging.info(f"{username} relay: selected number {relay_id}")
+        logging.info(f"{username} delay between on and off: {delay_in_seconds}s")
 
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
 
-def init(
-    username="alice",
-    relay_id=1,
-    delay_in_seconds=1,
-    mqtt_broker_ip="localhost",
-):
-    global _config
-    _config.name = username
-    _config.relay = relay_id
-    _config.delay = delay_in_seconds
-    _config.laser_channel = (
-        f"quantum_cryptography/classical_channel/{username}/laser"
+        self.client.connect(mqtt_broker_ip, 1883, 60)
+        self.client.loop_start()
+
+    logging.basicConfig(
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(filename="laser.log", mode="a"),
+        ],
+        format="%(asctime)s: %(levelname)s - %(message)s",
+        level=logging.INFO,
     )
-    logging.info(f"relay: selected number {relay_id}")
-    logging.info(f"delay between on and off: {delay_in_seconds}s")
-    client.connect(mqtt_broker_ip, 1883, 60)
-    client.loop_start()
+    logging.info("laser module: imported")
 
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info(f"mqtt: {self.name} connected")
 
-def on_connect(client, userdata, flags, rc):
-    logging.info(f"mqtt: {_config.name} connected")
+    def on_disconnect(self, client, userdata, rc):
+        logging.warning(f"mqtt: {self.name} disconnected")
 
+    def trigger(self):
+        self.client.publish(
+            self.laser_channel, payload="on", qos=0, retain=False
+        )
+        relay_lib_seeed.relay_on(self.relay)
+        logging.info(f"{self.name} laser relay {self.relay}: on")
 
-def on_disconnect(client, userdata, rc):
-    logging.warning(f"mqtt: {_config.name} disconnected")
+        time.sleep(self.delay)
 
-
-def trigger():
-    client.publish(_config.laser_channel, payload="on", qos=0, retain=False)
-    relay_lib_seeed.relay_on(_config.relay)
-    logging.info(f"laser relay {_config.relay}: on")
-
-    time.sleep(_config.delay)
-
-    client.publish(_config.laser_channel, payload="off", qos=0, retain=False)
-    relay_lib_seeed.relay_off(_config.relay)
-    logging.info(f"laser relay {_config.relay}: off")
-
-
-logging.basicConfig(
-    handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler(filename="laser.log", mode="a"),
-    ],
-    format="%(asctime)s: %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-logging.info("laser module: imported")
-
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
+        self.client.publish(
+            self.laser_channel, payload="off", qos=0, retain=False
+        )
+        relay_lib_seeed.relay_off(self.relay)
+        logging.info(f"{self.name} laser relay {self.relay}: off")
