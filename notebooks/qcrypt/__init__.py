438f8c5d607763e51feecdb04a33cfcc6608908f
Benjamin Medicke <bmedicke@gmail.com>
2021-05-24 14:41:45 +0200

    add laser documentation

---
+++ notebooks/qcrypt/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module is composed of a collection of submodules that can be used to demonstrate quantum cryptography.
+
+**qcrypt** provides the following functionality:
+
+* control hardware hardware (such as lasers and filter wheels)
+* handle the classical channel over MQTT
+* extensive logging of all processes
+"""
