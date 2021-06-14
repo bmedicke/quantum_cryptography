a7345bc3f7cbc9e2b56d6f128f0cd48b6badea55
Benjamin Medicke <bmedicke@gmail.com>
2021-06-14 22:08:10 +0200

    refactored code

--- notebooks/qcrypt/hardware/lightsensor.py
+++ notebooks/qcrypt/hardware/lightsensor.py
@@ -13,7 +13,8 @@ try:
 except ModuleNotFoundError as exception:
     print(f"{type(exception).__name__}: {exception}")
     HARDWARE_IMPORTS = False
-    
+
+
 class Lightsensor:
     logger = logging.getLogger("__Lightsensor__")
     formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s")
@@ -23,41 +24,51 @@ class Lightsensor:
     filehandler.setFormatter(formatter)
     logger.addHandler(streamhandler)
     logger.addHandler(filehandler)
-    
-    def __init__(self,
-           device="/dev/ttyACM0",
-           #mqtt_broker_ip="localhost",
-           log_level="INFO",
-           baud_rate=9600,
-           timeout=1
-          ):
+
+    def __init__(
+        self,
+        device="/dev/ttyACM0",
+        # mqtt_broker_ip="localhost",
+        log_level="INFO",
+        baud_rate=9600,
+        timeout=1,
+    ):
         self.device = device
-        #self.mqtt_broker_ip = mqtt_broker_ip
+        # self.mqtt_broker_ip = mqtt_broker_ip
         self.log_level = log_level
         self.baud_rate = baud_rate
         self.timeout = timeout
-        self.logger.setLevel(log_level)  
+        self.logger.setLevel(log_level)
         self.ser = self.getSerialConnection()
-        
-    def getSerialConnection(self): 
+
+    def getSerialConnection(self):
         try:
             print("establishing connection to lightsensor ..")
-            return serial.Serial(self.device, self.baud_rate, timeout=self.timeout)
+            return serial.Serial(
+                self.device, self.baud_rate, timeout=self.timeout
+            )
         except serial.SerialException:
             self.logger.error("Connection could not be established")
-       
+
+    # resets buffer and waits until it is filled up 30 and prints
     def getValuesArray(self):
         self.ser.reset_input_buffer()
-        while(self.ser.in_waiting < 30):
+        while self.ser.in_waiting < 30:
             pass
-        print(self.ser.in_waiting)
+        print("ser_in_waiting: " + str(self.ser.in_waiting))
         return self.ser.readline().decode("utf-8").rstrip().split(",\t")
-            
 
-   
-    
-    
- 
+    def getHighLow(self):
+        values = self.getValuesArray()
+        if len(values) < 0:
+            return False
 
-
-        
+        high_low = []
+        print(values)
+        if len(values) == 2:
+            for i in range(len(values)):
+                if int(values[i]) < 100:
+                    high_low.append("High")
+                elif int(values[i]) >= 100:
+                    high_low.append("Low")
+        return high_low
